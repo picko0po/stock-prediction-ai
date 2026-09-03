5f31a844dfc60e1f8cd988ab4404e0cbd04a60ef
picko0po <intelligentpick590@gmail.com>
2026-09-03 09:30:25 +0300

    Add files via upload

---
+++ model.py
@@ -0,0 +1,27 @@
+from torch import nn
+
+
+class LSTMStocksModule(nn.Module):
+    HIDDEN_SIZE = 2  # Number of LSTM hidden nodes
+    NUM_LAYERS = 1  # Number of LSTM layers
+    BIAS = True  # Whether to include the bias term for some of LSTM's equations
+
+    def __init__(self):
+        super(LSTMStocksModule, self).__init__()
+        self.lstm = nn.LSTM(
+            1,
+            self.HIDDEN_SIZE,
+            self.NUM_LAYERS,
+            self.BIAS,
+            batch_first=True
+        )
+        if self.HIDDEN_SIZE > 1:
+            self.linear = nn.Linear(self.HIDDEN_SIZE, 1, bias=False)
+
+    def forward(self, x):
+        _, (hidden, cell) = self.lstm(x.unsqueeze(-1))
+        out = hidden.squeeze()
+        if self.HIDDEN_SIZE > 1:
+            out = self.linear(out).squeeze()
+        return out
+
