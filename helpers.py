c02c5019bd3e2efded1eaaec56cce72a082cec3e
picko0po <intelligentpick590@gmail.com>
2026-09-04 13:41:37 +0300

    Refactor training and prediction functions in helpers.py

--- helpers.py
+++ helpers.py
@@ -1,77 +1,76 @@
-from functools import partial
+import random
 
 import numpy as np
-import pandas as pd
 import torch
+
 from torch.utils.data import DataLoader
 
 from model.dataset import LSTMStocksDataset
 from model.model import LSTMStocksModule
-from matplotlib import pyplot as plt
-from scipy.stats import norm
 
 
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 3e-4
 WEIGHT_DECAY = 1e-6
 
-
-def _draw_chart(y_series: pd.Series):
-    y_series.plot.hist(bins=50, label='Target Returns')
-
-    # Draw Gaussian curve
-    mu, stdev = norm.fit(y_series)
-    xmin, xmax = plt.xlim()
-    x = np.linspace(xmin, xmax, 50)
-    p = norm.pdf(x, mu, stdev)
-    p *= y_series.shape[0] / p.sum()
-
-    plt.plot(x, p, 'k', linewidth=2, label='Gaussian Curve')
-    plt.legend()
+BATCH_SIZE = 64
 
 
-def chart_y_histogram(y_series: pd.Series):
-    """
-    Create charts that contain the histogram of the inputs and compares them against Normal distributions.
-    :param y_series: Data to create charts for.
-    """
-    _draw_chart(y_series)
-    plt.show()
+def set_seed(seed=42):
 
-    _draw_chart(y_series)
-    xmin, _ = plt.xlim()
-    plt.axis([xmin, -0.015, 0, 10])
-    plt.show()
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
-
-def train(x_df: pd.DataFrame, y_series: pd.Series, epochs: int = 150):
-    """
-    Trains the LSTMStocksModule model
-    :param x_df: Inputs consisting of sequences of stock price returns
-    :param y_series: Targets consisting of returns some days in advance of the reference dates
-    :param epochs: Number of complete iterations to go through the data in order to train
-    :return: The trained LSTMStocksModule model
-    """
-    # Put data into GPU if possible
-    dataloader_kwargs = {}
     if torch.cuda.is_available():
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')  # Store all training data in the GPU
-        dataloader_kwargs['generator'] = torch.Generator(device='cuda')
-    else:
-        torch.set_default_tensor_type('torch.FloatTensor')
+        torch.cuda.manual_seed_all(seed)
 
-    # Turn pandas objects into Pytorch tensor objects
-    x_tensor, y_tensor = torch.tensor(x_df.values).float(), torch.tensor(y_series.values).float()
 
-    # Set up the dataloader
-    train_dataset = LSTMStocksDataset(x_tensor, y_tensor)
-    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True, **dataloader_kwargs)
+def train(
+    x_array,
+    y_array,
+    epochs=150
+):
 
-    model = LSTMStocksModule().train()
-    if torch.cuda.is_available():  # Train on GPU if possible
-        model = model.cuda()
+    set_seed(42)
 
-    loss_func = partial(torch.nn.functional.huber_loss, delta=0.02)
-    chart_y_histogram(y_series)
+    # Convert to tensors
+    x_tensor = torch.tensor(
+        x_array,
+        dtype=torch.float32
+    )
+
+    y_tensor = torch.tensor(
+        y_array,
+        dtype=torch.float32
+    )
+
+    # Dataset
+    train_dataset = LSTMStocksDataset(
+        x_tensor,
+        y_tensor
+    )
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True
+    )
+
+    # Device
+    device = torch.device(
+        "cuda" if torch.cuda.is_available()
+        else "cpu"
+    )
+
+    print("Training device:", device)
+
+    # Model
+    model = LSTMStocksModule().to(device)
+
+    # Huber loss
+    loss_func = torch.nn.HuberLoss(
+        delta=0.02
+    )
 
     optimizer = torch.optim.Adam(
         model.parameters(),
@@ -79,32 +78,76 @@ def train(x_df: pd.DataFrame, y_series: pd.Series, epochs: int = 150):
         weight_decay=WEIGHT_DECAY
     )
 
-    # Conduct training which consists of homing the model in on the best parameters that minimize the loss
-    for i in range(epochs):
-        total_loss = 0.
-        for x, y in train_dataloader:
-            optimizer.zero_grad()
-            out = model(x)
-            loss = loss_func(out, y)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.cpu().detach().numpy()
+    # Training
+    model.train()
 
-        print(f"[Epoch {i}] Loss: {total_loss:.4f}")
+    for epoch in range(epochs):
+
+        total_loss = 0.0
+
+        for x, y in train_dataloader:
+
+            x = x.to(device)
+            y = y.to(device)
+
+            optimizer.zero_grad()
+
+            prediction = model(x)
+
+            loss = loss_func(
+                prediction,
+                y
+            )
+
+            loss.backward()
+
+            # Prevent extremely large gradients
+            torch.nn.utils.clip_grad_norm_(
+                model.parameters(),
+                max_norm=1.0
+            )
+
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        average_loss = (
+            total_loss /
+            len(train_dataloader)
+        )
+
+        if (
+            epoch == 0
+            or (epoch + 1) % 10 == 0
+        ):
+            print(
+                f"Epoch {epoch + 1}/{epochs} "
+                f"- Loss: {average_loss:.6f}"
+            )
 
     return model
 
 
-def predict(trained_model, x_df: pd.DataFrame) -> pd.Series:
-    """
-    Generates predictions using a trained model
-    :param trained_model: Trained Pytorch model
-    :param x_df: Inputs to generate predictions for
-    :return: Series containing predictions, with reference dates as indices
-    """
+def predict(
+    trained_model,
+    x_array
+):
+
+    device = next(
+        trained_model.parameters()
+    ).device
+
+    x_tensor = torch.tensor(
+        x_array,
+        dtype=torch.float32
+    ).to(device)
+
     trained_model.eval()
 
-    x_tensor = torch.tensor(x_df.values).float()
-    prediction = trained_model(x_tensor)
+    with torch.no_grad():
 
-    return pd.Series(prediction.cpu().detach().numpy(), index=x_df.index)
+        prediction = trained_model(
+            x_tensor
+        )
+
+    return prediction.cpu().numpy()
